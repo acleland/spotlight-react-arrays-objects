6c176b74ad917cf833e21ca6309b2bf1c39fb01c
Andrea Cleland <andreaclelan@gmail.com>
2022-03-10 12:29:01 -0800

    implemented totalSneakerCount

--- index.js
+++ index.js
@@ -90,7 +90,13 @@ export const getBrandNames = (brands) => {
 
 // INPUT: brands from data.js
 // OUTPUT: total number of sneaker types across all brands (14)
-export const totalSneakerCount = () => {}
+export const totalSneakerCount = (brands) => {
+  let count = 0
+  for (let brand of Object.keys(brands)) {
+    count += brands[brand].shoes.length
+  }
+  return count
+}
 
 // INPUT: An object
 // OUTPUT: An array with key value pairs converted to arrays
